12700a4981deaf0bbd0b755dd6c895d2f553c3d3
Kanyesigye2002 <kanyeallan@gmail.com>
2024-06-03 10:48:50 +0300

    first commit

---
+++ composeApp/src/commonMain/kotlin/Greeting.kt
@@ -0,0 +1,10 @@
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+    
+    // create a sum function
+    
+}
